ea11c29762c0e5e692d1315ad57930b4543bf941
Jan <github@zjos.be>
2024-03-27 14:50:34 +0100

    wip - initiële structuur opgezet

---
+++ devdependencies/types/kris.d.ts
@@ -0,0 +1,25 @@
+export type MyType = {
+	name: string;
+};
+
+export type MyType2 = {
+	name: string;
+	code: string;
+	extra?: string;
+};
+
+export type MyKRISType = {
+	name: string;
+	code: string;
+	extra?: string;
+	url: string;
+	url2?: string;
+};
+
+export type MyKRISType2 = {
+	name: string;
+	code: string;
+	extra?: string;
+	url: string;
+	url2?: string;
+};
