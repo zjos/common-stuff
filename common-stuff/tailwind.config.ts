0bde2402548abb30d3f9b7eef3e4ba2d6ebe9361
Jan <github@zjos.be>
2024-04-01 09:27:58 +0200

    tailwind config aan package toegevoegd

---
+++ common-stuff/tailwind.config.ts
@@ -0,0 +1,47 @@
+import type { Config } from 'tailwindcss';
+import colors, { white } from 'tailwindcss/colors';
+
+const config: Config = {
+	content: [
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					'50': '#edfff5',
+					'100': '#d6ffe9',
+					'200': '#afffd3',
+					'300': '#71ffb3',
+					'400': '#2dfb8b',
+					'500': '#02e56b',
+					'600': '#00bf54',
+					DEFAULT: '#009b48',
+					'800': '#06753a',
+					'900': '#085f32',
+					'950': '#00361a',
+				},
+				'primary-foreground': colors.white,
+				secondary: {
+					'50': '#fff7ed',
+					'100': '#ffecd4',
+					'200': '#ffd5a9',
+					'300': '#ffb672',
+					'400': '#fe8d39',
+					DEFAULT: '#fd7622',
+					'600': '#ee5108',
+					'700': '#c53b09',
+					'800': '#9c2f10',
+					'900': '#7e2910',
+					'950': '#441106',
+				}, // Voeg hier je gewenste hex-kleurcode toe
+				thirdary: '#002776',
+				'primary-light': '#99DAD6',
+			},
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+	},
+};
+export default config;
